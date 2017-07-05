36b37cca3799bd868efd7cf5635133d9e5dec3cb
Margaret Walker <mwalker@Margarets-MacBook-Pro.local>
2017-07-05 14:45:31 -0400

    Finished FunWithTemplateTags

--- collection/views.py
+++ collection/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render
 def index(request):
     # defining the variable
     number = 6
+    thing = 'Thing name'
     # passing the variable to the view
     return render(request, 'index.html', {
         'number': number,
+        'thing': thing,
     })
