8f6b40883eea0c8310c843ede9d936da4ae2ac1b
Margaret Walker <mwalker@Margarets-MacBook-Pro.local>
2017-07-05 15:45:37 -0400

    Finished Ch8

--- collection/views.py
+++ collection/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from collection.models import Thing
 
-# Create your views here.
 def index(request):
-    # defining the variable
     things = Thing.objects.all()
-
-    # passing the variable to the view
     return render(request, 'index.html', {
         'things': things,
+    })
+
+# our new view
+def thing_detail(request, slug):
+    # grab the object...
+    thing = Thing.objects.get(slug=slug)
+
+    # and pass to the template
+    return render(request, 'things/thing_detail.html', {
+        'thing': thing,
     })
